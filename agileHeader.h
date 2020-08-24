b5df1e284a55699a87fbee8633f20fe949a37aeb
JTAmaral <jaredamaral1229@gmail.com>
2020-08-24 15:06:15 -0700

    Created header file for name functions

---
+++ agileHeader.h
@@ -0,0 +1,13 @@
+#ifndef HEADER_H_
+#define HEADER_H_
+
+//Preprocessor Directives
+#include <iostream>
+#include <string>
+#include <iomanip>
+using namespace std;
+
+//Prototypes
+void name1();
+
+#endif // !HEADER_H_
