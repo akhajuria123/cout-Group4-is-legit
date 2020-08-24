dfc12d91d6970b7d5041dabc39b4dc3a2f3b1a21
akhajuria123 <69995242+akhajuria123@users.noreply.github.com>
2020-08-23 19:50:21 -0700

    Update and rename Agile0 to Agile0.cpp

---
+++ Agile0.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include<Windows.h>
+using namespace std;
+
+void name_1()
+{
+cout << "Anish Khajuria \n" << endl;
+
+
+
+}
+
+
+   
+   
+
+
+int main()
+{
+
+
+name_1;
+
+
+
+
+return 0;
+}
