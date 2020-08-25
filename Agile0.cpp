defd97febdbbeebc63e4fb96b73ad2eda36c0f6c
mmccullough15 <69995730+mmccullough15@users.noreply.github.com>
2020-08-25 15:59:25 -0700

    Update Agile0.cpp

--- Agile0.cpp
+++ Agile0.cpp
@@ -1,8 +1,42 @@
 #include <iostream>
-
+#include<Windows.h>
 using namespace std;
 
+void name_1()
+{
+cout << "Anish Khajuria \n" << endl;
+
+
+
+}
+
 void printMyName(){
-    cout << "Matthew McCullough\n\n";
-    return;
+	cout << "Matthew McCullough\n\n";
+	return;
+}
+   
+void name_3()
+{
+	cout << "Connor Redemske\n" << endl;
+}
+
+void name_4()
+{
+	cout << "Jared Amaral\n";
+}
+
+int main()
+{
+
+
+name_1();
+printMyName();
+name_3();
+name_4();
+
+
+
+cout << endl << endl << "Thanks for using our program." << endl;
+
+return 0;
 }
