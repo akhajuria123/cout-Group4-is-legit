3e32c49358e809411db2fd4d35191639f80de330
ConroyHilton <69995849+ConroyHilton@users.noreply.github.com>
2020-08-25 16:09:24 -0700

    Update Agile0.cpp

--- Agile0.cpp
+++ Agile0.cpp
@@ -1,9 +1,43 @@
 #include <iostream>
-
+#include<Windows.h>
 using namespace std;
 
-//prints name "Connor Redemske";
+void name_1()
+{
+cout << "Anish Khajuria \n" << endl;
+
+
+
+}
+
+void printMyName(){
+	cout << "Matthew McCullough\n\n";
+	return;
+}
+   
 void name_3()
 {
 	cout << "Connor Redemske\n" << endl;
+}
+
+//Prints "Jared Amaral"
+void name_4()
+{
+	cout << "Jared Amaral\n";
+}
+
+int main()
+{
+
+
+name_1();
+printMyName();
+name_3();
+name_4();
+
+
+
+cout << endl << endl << "Thanks for using our program." << endl;
+
+return 0;
 }
