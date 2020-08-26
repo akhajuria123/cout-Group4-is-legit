37ee886af0b07d35a12b31486161badf72d3a789
mmccullough15 <69995730+mmccullough15@users.noreply.github.com>
2020-08-25 21:49:40 -0700

    Update feature1.h

--- feature1.h
+++ feature1.h
@@ -1,7 +1,15 @@
+  
 #ifndef FEATURE1_H
 #define FEATURE1_H
 
-void printMyName();
+#include <iostream>
+#include <windows.h>
+#include <string>
+using namespace std;
+
+void name_1();
+void name_2();
+void name_3();
+void name_4();
 
 #endif
-
